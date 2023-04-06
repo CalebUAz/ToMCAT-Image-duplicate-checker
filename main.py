6a193e3df98881e64b565d165c35138eee76eb13
CalebUAz <calebshibu@email.arizona.edu>
2023-04-06 15:19:43 -0700

    Look at a list of 10 images rather than a pair at a time

--- main.py
+++ main.py
@@ -24,30 +24,35 @@ def images_are_similar_hash(image1_path, image2_path, csv_file_path):
             writer = csv.writer(csv_file)
             writer.writerow(["images_are_similar_hash", image1_path, image2_path])
 
-# Define a function to run the comparison functions on a pair of images
-def process_pair(image_paths, csv_file_path):
-    images_are_similar_histogram(image_paths[0], image_paths[1], csv_file_path)
-    images_are_similar_hash(image_paths[0], image_paths[1], csv_file_path)
+# Define a function to run the comparison functions on a list of images
+def process_list(image_paths, csv_file_path):
+    for i in range(len(image_paths)):
+        for j in range(i+1, len(image_paths)):
+            images_are_similar_histogram(image_paths[i], image_paths[j], csv_file_path)
+            images_are_similar_hash(image_paths[i], image_paths[j], csv_file_path)
 
 # Define a function to delete similar images using parallel processing
-def delete_similar_images(directory, csv_file_path, num_processes=None):
+def delete_similar_images(directory, csv_file_path, num_processes=None, batch_size=10):
     image_paths = sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.png')])
 
-    # Split the list of image paths into chunks for parallel processing
+    # Split the list of image paths into batches for parallel processing
     if num_processes is None:
         num_processes = multiprocessing.cpu_count()
-    chunk_size = len(image_paths) // num_processes
-    image_path_chunks = [image_paths[i:i+chunk_size] for i in range(0, len(image_paths), chunk_size)]
+    batch_size = min(batch_size, len(image_paths))
+    num_batches = (len(image_paths) + batch_size - 1) // batch_size
+    image_path_batches = [image_paths[i*batch_size:(i+1)*batch_size] for i in range(num_batches)]
 
-    # Run the comparison functions on each pair of images in parallel processes
+    # Run the comparison functions on each list of images in parallel processes
     with multiprocessing.Pool(num_processes) as pool:
-        for chunk in image_path_chunks:
-            args_list = [(pair, csv_file_path) for pair in zip(chunk, chunk[1:])]
-            pool.starmap(process_pair, args_list)
+        for batch in image_path_batches:
+            pool.apply_async(process_list, args=(batch, csv_file_path))
+        pool.close()
+        pool.join()
 
 # Example usage
 csv_file_path = "similar_images.csv"
 with open(csv_file_path, "w", newline="") as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["method", "image1_path", "image2_path"])
-delete_similar_images("/space/tomcat/LangLab/experiments/study_3_pilot/group/exp_2023_02_21_14/lion/face_images", csv_file_path, num_processes=4)
+delete_similar_images("/space/tomcat/LangLab/experiments/study_3_pilot/group/exp_2023_02_21_14/lion/face_images", csv_file_path, num_processes=4, batch_size=10)
+
