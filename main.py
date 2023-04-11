fffcaaad19eb701299f7daf97646fda90f4adb9f
CalebUAz <calebshibu@email.arizona.edu>
2023-04-11 01:09:58 -0700

    option to enter file name and delete images

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import csv
 import imagehash
 import multiprocessing
 import numpy as np
+import argparse
 
-def images_are_similar_mse(image1_path, image2_path, csv_file_path):
+def images_are_similar_mse(image1_path, image2_path, csv_file_path, delete_images):
     image1 = Image.open(image1_path).resize((100, 100), Image.ANTIALIAS)
     image2 = Image.open(image2_path).resize((100, 100), Image.ANTIALIAS)
     image1_np = np.asarray(image1)[:,:,0]
@@ -13,15 +14,18 @@ def images_are_similar_mse(image1_path, image2_path, csv_file_path):
     mse = np.mean((image1_np.astype("float") - image2_np.astype("float")) ** 2)
     print(mse)
     if mse == 0:
-        with open(csv_file_path, "a", newline="") as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([image1_path])
+        if delete_images:
+            os.remove(image1_path)
+        else:
+            with open(csv_file_path, "a", newline="") as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow([image1_path])
 
 def worker(args):
-    image_paths, method, csv_file_path = args
-    process_list(image_paths, method, csv_file_path)
+    image_paths, method, csv_file_path, delete_images = args
+    process_list(image_paths, method, csv_file_path, delete_images)
 
-def delete_similar_images(directory, num_processes=None, batch_size=10):
+def delete_similar_images(directory, delete_images, num_processes=None, batch_size=10):
     csv_files = {"images_are_similar_mse": "duplicate_images.csv"}
     
     image_paths = sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.png')])
@@ -41,23 +45,34 @@ def delete_similar_images(directory, num_processes=None, batch_size=10):
             writer.writerow(["image_path"])
 
     with multiprocessing.Pool(num_processes) as pool:
-        tasks = [(batch, method, csv_file_path) for batch in image_path_batches for method, csv_file_path in csv_file_paths.items()]
+        tasks = [(batch, method, csv_file_path, delete_images) for batch in image_path_batches for method, csv_file_path in csv_file_paths.items()]
         for _ in pool.imap_unordered(worker, tasks):
             pass
         pool.close()
         pool.join()
 
-def process_list(image_paths, method, csv_file_path):
+def process_list(image_paths, method, csv_file_path, delete_images):
     for i in range(len(image_paths)):
         for j in range(i+1, len(image_paths)):
             if method == "images_are_similar_mse":
-                images_are_similar_mse(image_paths[i], image_paths[j], csv_file_path)
+                images_are_similar_mse(image_paths[i], image_paths[j], csv_file_path, delete_images)
 
+    for i in range(len(image_paths)):
+        for j in range(i+1, len(image_paths)):
+            if method == "images_are_similar_mse":
+                images_are_similar_mse(image_paths[i], image_paths[j], csv_file_path, delete_images)
 
 if __name__ == '__main__':
-    base_dir = "/Users/calebjonesshibu/Desktop/tom/exp_2023_02_21_14"
+    parser = argparse.ArgumentParser(description='Find and remove duplicate images.')
+    parser.add_argument('--exp_dir', type=str, required=True, help='Experiment directory path which has lion, tiger, leopard which contains the image folders.')
+    parser.add_argument('--delete', action='store_true', help='Delete duplicate images instead of writing to CSV.')
+    args = parser.parse_args()
+    
+    exp_dir = str(args.exp_dir)
+    delete_images = args.delete
     imac_folders = ["lion", "tiger", "leopard"]
     
     for imac in imac_folders:
-        folder_path = os.path.join(base_dir, imac, "face_images")
-        delete_similar_images(folder_path, num_processes=10, batch_size=2)
+        folder_path = os.path.join(exp_dir, imac, "face_images")
+        delete_similar_images(folder_path, num_processes=10, batch_size=2, delete_images=delete_images)
+        
